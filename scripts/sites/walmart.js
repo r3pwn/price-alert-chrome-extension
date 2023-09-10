a5e96836a5cb64fea3879f7ce53efbf06481f3ee
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-09-10 15:55:54 -0400

    feat(walmart): console log product data

--- scripts/sites/walmart.js
+++ scripts/sites/walmart.js
@@ -5,4 +5,9 @@
 
   // for debugging purposes. remove later
   priceBadge.classList.add('visually-distinct-price-badge');
+
+  const nextJsData = JSON.parse((await utils.waitForElement('#__NEXT_DATA__')).textContent);
+  const productData = nextJsData?.props?.pageProps?.initialData?.data?.product;
+
+  console.log('productData', productData);
 })()
