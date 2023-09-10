6ee0f8a99073c81a768879897f65aa1a0e363f20
Daniel Tejeda <daviddan1998@gmail.com>
2023-09-10 12:27:59 -0400

    feat(popup): add api fetching

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,11 @@
-//Document Hooks
-const $listwrapper = $('#list-wrapper')
+//CONSTANTS
+const API_URL = 'https://mocki.io/v1/c3e9cbec-0bb1-4481-9913-197cb355329e'
+const TEMPLATES = '../templates/'
 const STORES = {
+    PUBLIX: {
+        name: "Publix",
+        image: "/images/store-logos/Publix-Logo.png"
+    },
     KROGER: {
         name: "Kroger",
         image: "/images/store-logos/Kroger-Logo.png"
@@ -14,27 +19,44 @@ const STORES = {
         image: "/images/store-logos/WALMART-Logo.png"
     }
 }
-//templates 
-const TEMPLATES = '../templates/'
-const data = [{name: 'walmart'}, {name: 'target'}];
 
-if (!data.length) {
-    $listwrapper.load(`${TEMPLATES}/NoPricesFound.html`)
+//Document Hooks
+const $listwrapper = $('#list-wrapper')
+
+const getData = async () => {
+    let data = {};
+    try {
+        data = await (await fetch(API_URL)).json()
+    }
+    catch (err) {
+        if (err) {
+            $listwrapper.load(`${TEMPLATES}/ErrorOnRequest.html`);
+            return;
+        }
+    }
+    if (!data.stores?.length) {
+        $listwrapper.load(`${TEMPLATES}/NoPricesFound.html`)
+    }
+    
+    data.stores.forEach(store => {
+        const $tempElement = $("<div></div>");
+        $tempElement.load(`${TEMPLATES}/StorePriceInfo.html`, () => {
+            const $listElement = $tempElement.find(".store")
+            const $image = $tempElement.find(".store-image")[0]
+            const $storeName = $tempElement.find(".store-name")[0]
+            const $percentage = $tempElement.find(".price-percentage")[0]
+            const $price = $tempElement.find(".price")[0]
+    
+            $image.src = STORES[store.storeName.toUpperCase()]?.image;
+            $image.alt = STORES[store.storeName.toUpperCase()]?.name;
+            $storeName.innerText = STORES[store.storeName.toUpperCase()]?.name;
+
+            $price.innerText = `$${store.price}`
+    
+            $(".store-list").append($listElement);
+        });
+    })
 }
 
-data.forEach(store => {
-    const $tempElement = $("<div></div>");
-    $tempElement.load(`${TEMPLATES}/StorePriceInfo.html`, () => {
-        const $listElement = $tempElement.find(".store")
-        const $image = $tempElement.find(".store-image")[0]
-        const $storeName = $tempElement.find(".store-name")[0]
-        const $percentage = $tempElement.find(".price-percentage")[0]
-        const $price = $tempElement.find(".price")[0]
+getData();
 
-        $image.src = STORES[store.name.toUpperCase()].image;
-        $image.alt = STORES[store.name.toUpperCase()].name;
-        $storeName.innerText = STORES[store.name.toUpperCase()].name;
-
-        $(".store-list").append($listElement);
-    });
-})
