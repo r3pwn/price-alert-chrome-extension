cd0fbcd40ec584fde809d1c54e8ec1fdd3cc284d
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-09-12 01:04:25 -0400

    feat(target): look up current product by TCIN

--- scripts/sites/target.js
+++ scripts/sites/target.js
@@ -1,8 +1,20 @@
 (async function main() {
+  const getTcinFromUrl = () => {
+    return window.location.href.split('/').pop().match(/([0-9])+/)[0];
+  }
+
   const utils = await import('../utils.js');
 
+  const tcin = getTcinFromUrl();
   const priceBadge = await utils.waitForElement('[data-test="product-price"]');
 
   // for debugging purposes. remove later
   priceBadge.classList.add('visually-distinct-price-badge');
+
+  const returnedProducts = await utils.getProductBy('TCIN', tcin);
+
+  const relevantProduct = returnedProducts?.[0];
+
+  console.log('found product', relevantProduct.productName)
+  console.table(relevantProduct.prices)
 })()
