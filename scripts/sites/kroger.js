d62511380e076fb86bd47c14d8a0030a4b009c47
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-09-12 16:39:42 -0400

    feat(kroger): lookup products by UPC

--- scripts/sites/kroger.js
+++ scripts/sites/kroger.js
@@ -1,8 +1,31 @@
 (async function main() {
+  const getUpcFromUrl = () => {
+    return window.location.href.split('/').pop().match(/([0-9])+/)[0];
+  }
+
   const utils = await import('../utils.js');
 
+  const upc = getUpcFromUrl();
   const priceBadge = await utils.waitForElement('[data-qa="cart-page-item-unit-price"]');
 
   // for debugging purposes. remove later
   priceBadge.classList.add('visually-distinct-price-badge');
+
+  let returnedProduct;
+
+  await utils.getProductBy('UPC', upc)
+    .then((response) => {
+      returnedProduct = response?.[0];
+    })
+    .catch(() => {
+      console.log('no product found for upc', upc);
+    });
+  
+  if (!returnedProduct) {
+    // we don't have info on this product, we can skip it for now
+    return;
+  }
+
+  console.log('found product', returnedProduct.productName)
+  console.table(returnedProduct.prices)
 })()
