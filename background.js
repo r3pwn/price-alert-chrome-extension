9b5195b92ca2daeb1603aaaba1025c073af27b5a
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-09-15 00:28:05 -0400

    fix(background): resolve issues w/ scripts not injecting

--- background.js
+++ background.js
@@ -4,19 +4,23 @@ const SITES = {
   'www.walmart.com/ip/': 'scripts/sites/walmart.js'
 }
 
-// When the user clicks on the extension action
-chrome.action.onClicked.addListener(async (tab) => {
-  console.log('icon was clicked!')
-});
-
 chrome.runtime.onMessage.addListener((request, sender) => {
   if(request.sender?.includes('price-analysis')) {
     chrome.storage.local.set({"item": JSON.stringify(request)})
   }
 })
 
+let activeUrl = '';
+
 for (let path of Object.keys(SITES)) {
   const listener = async (details) => {
+    if (details.url === activeUrl) {
+      // we've already run and injected, so we can safely return.
+      return;
+    }
+
+    activeUrl = details.url;
+
     await chrome.scripting.insertCSS({
       files: ['price-badge.css'],
       target: { tabId: details.tabId }
@@ -33,7 +37,10 @@ for (let path of Object.keys(SITES)) {
   /* several e-commerce sites seem to be using some kind of router, so make sure we fire 
    * on full page loads as well as "soft" page loads
    */
-  // this one may not be needed, TBD
-  // chrome.webNavigation.onCompleted.addListener(listener, filter);
+  chrome.webNavigation.onDOMContentLoaded.addListener(listener, filter);
   chrome.webNavigation.onHistoryStateUpdated.addListener(listener, filter);
+
+  // onCompleted and onErrorOccurred, we want to reset the activeUrl so the extension will load again
+  chrome.webNavigation.onCompleted.addListener(() => { activeUrl = ''; }, filter);
+  chrome.webNavigation.onErrorOccurred.addListener(() => { activeUrl = ''; }, filter);
 }
