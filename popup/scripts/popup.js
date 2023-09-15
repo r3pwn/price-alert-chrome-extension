c52ae1cea9a093d5869dee4ae7fcee14434dd9b1
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-09-15 01:13:57 -0400

    fix(popup): don't display old data

--- popup/scripts/popup.js
+++ popup/scripts/popup.js
@@ -1,63 +1,67 @@
-//CONSTANTS
 const TEMPLATES = './templates/'
-const STORES = {
-    PUBLIX: {
-        name: "Publix",
-        image: "/images/store-logos/Publix-Logo.png"
-    },
-    KROGER: {
-        name: "Kroger",
-        image: "/images/store-logos/Kroger-Logo.png"
-    },
-    TARGET: {
-        name: "Target",
-        image: "/images/store-logos/Target-Logo.png"
-    },
-    WALMART: {
-        name: "Walmart",
-        image: "/images/store-logos/WALMART-Logo.png"
-    }
-}
+const STORES = [
+  {
+    name: 'Kroger',
+    image: '/images/store-logos/Kroger-Logo.png',
+    path: 'https://www.kroger.com/p/'
+  },
+  {
+    name: 'Target',
+    image: '/images/store-logos/Target-Logo.png',
+    path: 'https://www.target.com/p/'
+  },
+  {
+    name: 'Walmart',
+    image: '/images/store-logos/WALMART-Logo.png',
+    path: 'https://www.walmart.com/ip/'
+  }
+]
 
 //Document Hooks
 const $listwrapper = $('#list-wrapper')
 
 // Are we in a supported site?
-let inSupportedStoreSite = false;
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    inSupportedStoreSite = Object.keys(STORES)
-        .some(key => tabs[0].url.includes(key.toLowerCase()))
-        
-        if (!inSupportedStoreSite) {
-            console.error('Store not supported!')
-            renderNoPricesFound();
-            return;
-        }
-        chrome.storage.local.get(["item"]).then(result => populateData(JSON.parse(result.item).product))
+  let onSupportedPage = STORES.some(store => tabs[0].url.startsWith(store.path))
+      
+  if (!onSupportedPage) {
+    renderNotSupportedPage();
+    return;
+  }
+
+  chrome.storage.local.get(["item"]).then(result => populateData(JSON.parse(result.item).product))
 })
 
-
 const populateData = (productData) => {
-    if (!productData?.stores?.length) {
-        renderNoPricesFound();
-        return;
+  if (!productData?.stores?.length) {
+    renderNoPricesFound();
+    return;
+  }
+  
+  $('.product-title').text(productData.productName);
+
+  for (let store of productData.stores) {
+    const storeData = STORES.find(supportedStore => supportedStore.name.toLowerCase() === store.name.toLowerCase());
+    if (!storeData) {
+      // we don't support displaying this store's values (yet)
+      continue;
     }
-    
-    $('.product-title').text(productData.productName);
-    
-    productData.stores.forEach(store => {
-        const productCard = document.createElement('store-product-card')
-        productCard.setAttribute('id', 'product-' + STORES[store.name.toUpperCase()]?.name)
-        productCard.product = {
-            image: STORES[store.name.toUpperCase()]?.image,
-            imageAlt: STORES[store.name.toUpperCase()]?.name,
-            store: STORES[store.name.toUpperCase()]?.name,
-            price: store.price.toFixed(2)
-        }
-        $(".store-list").append(productCard)
-    })
+    const productCard = document.createElement('store-product-card')
+    productCard.setAttribute('id', `product-${storeData.name}`)
+    productCard.product = {
+      image: storeData.image,
+      imageAlt: `${storeData.name} logo`,
+      store: storeData.name,
+      price: store.price.toFixed(2)
+    }
+    $('.store-list').append(productCard);
+  }
+}
+
+const renderNotSupportedPage = () => {
+  $listwrapper.load(`${TEMPLATES}/NotOnProductPage.html`)
 }
 
 const renderNoPricesFound = () => {
-    $listwrapper.load(`${TEMPLATES}/NoPricesFound.html`)
+  $listwrapper.load(`${TEMPLATES}/NoPricesFound.html`)
 }
